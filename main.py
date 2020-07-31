3b8035cf44f04a0423787438c53a568d506d93c8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-07-31 17:16:31 +0900

    4.Waiting for Elements

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 KEYWORD = "buy domain"
@@ -13,12 +16,20 @@ search_bar = browser.find_element_by_class_name("gLFyf")
 search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
-search_results = browser.find_elements_by_class_name("g")
+
+shitty_element = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
+)
+
+print(shitty_element)
+
+
+""" search_results = browser.find_elements_by_class_name("g")
 
 for index, search_result in enumerate(search_results):
     class_name = search_result.get_attribute("class")
     if "kno-kp mnr-c g-blk" not in class_name:
         search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+ """
 
-
-browser.quit()
+# browser.quit()
