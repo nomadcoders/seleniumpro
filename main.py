f8798e44e5288e1d14d6b52e74bb3497759140dd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-07-31 17:42:12 +0900

    6.Refactor, Recap, Code Challenge

--- main.py
+++ main.py
@@ -5,33 +5,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-KEYWORD = "buy domain"
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+class GoogleKeywordScreenshooter:
+    def __init__(self, keyword, screenshots_dir):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.keyword = keyword
+        self.screenshots_dir = screenshots_dir
 
-browser.get("https://google.com")
+    def start(self):
+        self.browser.get("https://google.com")
+        search_bar = self.browser.find_element_by_class_name("gLFyf")
+        search_bar.send_keys(self.keyword)
+        search_bar.send_keys(Keys.ENTER)
+        try:
+            shitty_element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
+            )
+            self.browser.execute_script(
+                """
+            const shitty = arguments[0];
+            shitty.parentElement.removeChild(shitty)
+            """,
+                shitty_element,
+            )
+        except Exception:
+            pass
+        search_results = self.browser.find_elements_by_class_name("g")
+        for index, search_result in enumerate(search_results):
+            search_result.screenshot(
+                f"{self.screenshots_dir}/{self.keyword}x{index}.png"
+            )
 
-search_bar = browser.find_element_by_class_name("gLFyf")
-
-search_bar.send_keys(KEYWORD)
-search_bar.send_keys(Keys.ENTER)
+    def finish(self):
+        self.browser.quit()
 
 
-shitty_element = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
-)
-
-browser.execute_script(
-    """
-const shitty = arguments[0];
-shitty.parentElement.removeChild(shitty)
-""",
-    shitty_element,
-)
-
-search_results = browser.find_elements_by_class_name("g")
-
-for index, search_result in enumerate(search_results):
-    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
-
-browser.quit()
+domain_competitors = GoogleKeywordScreenshooter("buy domain", "screenshots")
+domain_competitors.start()
+domain_competitors.finish()
+python_competitors = GoogleKeywordScreenshooter("python book", "screenshots")
+python_competitors.start()
+python_competitors.finish()
