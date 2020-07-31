46237b5d11032d68b585046f8c57b0d657aeb198
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-07-31 16:37:16 +0900

    3.Taking Screenshots

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
+KEYWORD = "buy domain"
+
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
 browser.get("https://google.com")
 
 search_bar = browser.find_element_by_class_name("gLFyf")
 
-search_bar.send_keys("hello!")
+search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
 search_results = browser.find_elements_by_class_name("g")
 
-for search_result in search_results:
-    title = search_result.find_element_by_tag_name("h3")
-    if title:
-        print(title.text)
+for index, search_result in enumerate(search_results):
+    class_name = search_result.get_attribute("class")
+    if "kno-kp mnr-c g-blk" not in class_name:
+        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+
+
+browser.quit()
