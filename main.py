e85f6c4d56f24e5445539278dcf25081e8c9f658
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-07-31 16:22:47 +0900

    1.Searching

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
+browser = webdriver.Chrome(ChromeDriverManager().install())
 
 browser.get("https://google.com")
+
+search_bar = browser.find_element_by_class_name("gLFyf")
+
+search_bar.send_keys("hello!")
+search_bar.send_keys(Keys.ENTER)
+
+search_results = browser.find_elements_by_class_name("g")
+
+print(search_results)
