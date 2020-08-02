263c1bb557ec13c8141f12fdd1ff076065c62637
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-08-02 15:54:36 +0900

    2.Counting Posts

--- instamining.py
+++ instamining.py
@@ -9,23 +9,38 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
+
+def wait_for(locator):
+    return WebDriverWait(browser, 10).until(EC.presence_of_element_located(locator))
+
+
 main_hashtag = "dog"
 
 browser.get(f"https://www.instagram.com/explore/tags/{main_hashtag}")
 
-header = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.TAG_NAME, "header"))
-)
+header = wait_for((By.TAG_NAME, "header"))
+
 hashtags = header.find_elements_by_class_name("AC7dP")
 
 for hashtag in hashtags:
     ActionChains(browser).key_down(Keys.COMMAND).click(hashtag).perform()
 
 
+counted_hashtags = []
+used_hashtags = []
+
 for window in browser.window_handles:
     browser.switch_to.window(window)
-    hashtag_name = browser.find_element_by_tag_name("h1")
-    print(hashtag_name.text[1:])
+    hashtag_name = wait_for((By.TAG_NAME, "h1"))
+    post_count = wait_for((By.CLASS_NAME, "g47SY"))
+    if post_count:
+        post_count = int(post_count.text.replace(",", ""))
+    if hashtag_name:
+        hashtag_name = hashtag_name.text[1:]
+    if hashtag_name and post_count:
+        if hashtag_name not in used_hashtags:
+            counted_hashtags.append((hashtag_name, post_count))
+            used_hashtags.append(hashtag_name)
 
 time.sleep(3)
 browser.quit()
