93ad5522dca428279eb88b5533d5dfcc23dcf7b8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-08-02 16:20:43 +0900

    4.It works

--- instamining.py
+++ instamining.py
@@ -7,8 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-
-max_hashtags = 20
+max_hashtags = 15
 browser = webdriver.Chrome(ChromeDriverManager().install())
 counted_hashtags = []
 used_hashtags = []
@@ -18,13 +17,17 @@ def wait_for(locator):
     return WebDriverWait(browser, 10).until(EC.presence_of_element_located(locator))
 
 
+def clean_hashtag(hashtag):
+    return hashtag[1:]
+
+
 def extract_data():
     hashtag_name = wait_for((By.TAG_NAME, "h1"))
     post_count = wait_for((By.CLASS_NAME, "g47SY"))
     if post_count:
         post_count = int(post_count.text.replace(",", ""))
     if hashtag_name:
-        hashtag_name = hashtag_name.text[1:]
+        hashtag_name = clean_hashtag(hashtag_name.text)
     if hashtag_name and post_count:
         if hashtag_name not in used_hashtags:
             counted_hashtags.append((hashtag_name, post_count))
@@ -36,11 +39,14 @@ def get_related(target_url):
     header = wait_for((By.TAG_NAME, "header"))
     hashtags = header.find_elements_by_class_name("AC7dP")
     for hashtag in hashtags:
-        ActionChains(browser).key_down(Keys.COMMAND).click(hashtag).perform()
+        hashtag_name = clean_hashtag(hashtag.text)
+        if hashtag_name not in used_hashtags:
+            ActionChains(browser).key_down(Keys.COMMAND).click(hashtag).perform()
 
     for window in browser.window_handles:
         browser.switch_to.window(window)
         extract_data()
+        time.sleep(1)
 
     if len(used_hashtags) < max_hashtags:
         for window in browser.window_handles[0:-1]:
@@ -52,3 +58,4 @@ def get_related(target_url):
 
 initial_hashtag = "dog"
 get_related(f"https://www.instagram.com/explore/tags/{initial_hashtag}")
+print(counted_hashtags)
